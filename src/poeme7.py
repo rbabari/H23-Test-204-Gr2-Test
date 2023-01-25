cb46d804bd040f5012f934d48e74500997c07537
Chrisserver <66754560+Chrisserver@users.noreply.github.com>
2023-01-25 14:11:39 -0500

    Create poeme7.py
    Poème Arthur Rimbaud

---
+++ src/poeme7.py
@@ -0,0 +1,4 @@
+print("L'hiver, nous partirons dans un petit wagon rose \n
+aux coussins bleus. Nous serons à l'aise. \n
+Un nid de baisers fous gît Dans chaque recoin moelleux.")
+print("https://www.best-poems.net/arthur-rimbaud/dream-winter.html")
